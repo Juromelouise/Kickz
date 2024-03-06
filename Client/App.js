0efd408fd74899620d45b95ef93a7065cd10e3bc
Heironymus <123807319+Juromelouise@users.noreply.github.com>
2024-03-06 10:23:40 +0800

    expo network and ip add

--- Client/App.js
+++ Client/App.js
@@ -1,10 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import * as Network from "expo-network";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [ipAddress, setIpAddress] = useState(null);
+
+  const getLocalIpAddress = async () => {
+    try {
+      const ipAddress = await Network.getIpAddressAsync();
+      setIpAddress(ipAddress || 'N/A');
+    } catch (error) {
+      console.error('Error getting IP address:', error.message);
+    }
+  };
+
+  useEffect(() => {
+    getLocalIpAddress();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>IP Address: {ipAddress}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +31,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
